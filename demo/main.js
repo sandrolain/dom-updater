74e9afd39ea68fa3bd3e07d3b14a98a33fdf875c
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-06-13 15:01:53 +0200

    Added event listener automatic management for DOMController

--- demo/main.js
+++ demo/main.js
@@ -24,6 +24,7 @@ const getNewHTML = () => (/*html*/`
 <div>
   <div><em>BAR</em></div>
   <hr>
+  <button id="myid">Hello World!</button>
 </div>
 `);
 
@@ -31,15 +32,15 @@ const getNewTree = () => {
   return htmlToDOMFragment(getNewHTML());
 };
 
-function updateWithInnerHTML (el, html) {
-  return new Promise((resolve) => {
-    window.requestAnimationFrame(() => {
-      const start = performance.now();
-      el.innerHTML = html;
-      resolve(performance.now() - start);
-    });
-  });
-}
+// function updateWithInnerHTML (el, html) {
+//   return new Promise((resolve) => {
+//     window.requestAnimationFrame(() => {
+//       const start = performance.now();
+//       el.innerHTML = html;
+//       resolve(performance.now() - start);
+//     });
+//   });
+// }
 
 const render = async () => {
   console.clear();
@@ -59,33 +60,55 @@ const render = async () => {
 // setInterval(render, 3000);
 
 
-document.addEventListener("click", () => {
-  for(let i = 0; i < 10; i++) {
-    list.push(Math.random());
-  }
-  render();
-});
+// document.addEventListener("click", () => {
+//   for(let i = 0; i < 10; i++) {
+//     list.push(Math.random());
+//   }
+//   render();
+// });
 
 DOMController.setTemplateArgument("foo", "bar");
 
 
 new DOMController({
   element: document.getElementById("ctrl"),
+  template: /*html*/`
+  <div id="ctrl">
+    \${"test.num"}<br/>
+    \${(vars) => (vars.test.num * 2).toFixed(2)}<br/>
+    \${() => foo}
+    \${(vars) => vars.list.map((value) => \`<div class="item">\${value}</div>\`)}
+    <button>Reset</button>
+  </div>
+  `,
   initialState: {
     test: {
       num: 0
     },
     aaa: {
       prova: 0
-    }
+    },
+    list: []
   },
   init: function (state) {
+    this.addListener(".item", "click", function () {
+      console.log(this);
+    });
+    this.addListener("#ctrl button", "click", function () {
+      state.list = [];
+    });
+
     state.test.num = 999;
     setInterval(() => {
       state.test.num = Math.random();
       state.test.num = Math.random();
       state.test.num = Math.random();
       console.log("state.test.num", state.test.num);
+      state.list.push(state.test.num);
     }, 1000);
   }
+});
+
+document.getElementById("myid").addEventListener("click", () => {
+  alert("Hello World!");
 });
--- src/DOMController.ts
+++ src/DOMController.ts
@@ -5,7 +5,7 @@ import { updateDOM } from "./DOMUpdater";
 export interface DOMControllerArguments<T> {
   element: HTMLElement;
   initialState?: Record<string, T>;
-  templateHTML?: string;
+  template?: string | DOMTemplateBuilder;
   init?: (stateProxy: ProxyHandler<Record<string, T>>) => void;
 }
 
@@ -15,17 +15,18 @@ export class DOMController<T=any> {
   private rafRequest: number;
   private node: HTMLElement;
   private stateObj: Record<string, T> = {};
-  private templateHTML?: string;
+  private template?: string | DOMTemplateBuilder;
   private controllerFn?: (stateProxy: ProxyHandler<Record<string, T>>) => void;
+  private listeners: Map<string, Map<string, Set<(e: Event) => any>>> = new Map();
 
-  constructor ({ element, initialState, templateHTML, init }: DOMControllerArguments<T>) {
+  constructor ({ element, initialState, template, init }: DOMControllerArguments<T>) {
     this.node         = element;
     this.stateObj     = initialState || {};
-    this.templateHTML = templateHTML;
+    this.template     = template;
     this.controllerFn = init;
 
-    if(!this.templateHTML) {
-      this.templateHTML = this.node.outerHTML;
+    if(!this.template) {
+      this.template = this.node.outerHTML;
     }
     this.updateTemplateBuilder();
     this.state = watch(this.stateObj, () => {
@@ -39,16 +40,83 @@ export class DOMController<T=any> {
   }
 
   setTemplateHTML (templateHTML: string): void {
-    this.templateHTML = templateHTML;
+    this.template = templateHTML;
     this.updateTemplateBuilder();
   }
 
   updateTemplateBuilder (): void {
-    this.builder = getTemplateBuilder(this.templateHTML, DOMController.getTemplateArguments());
+    if(typeof this.template === "string") {
+      this.builder = getTemplateBuilder(this.template, DOMController.getTemplateArguments());
+    } else if(this.template instanceof DOMTemplateBuilder) {
+      this.builder = this.template;
+    }
   }
 
   setState (newState: Record<string, T>): void {
     Object.assign(this.state, newState);
+  }
+
+  private getSelectorListeners (selector: string, eventName: string): Set<(e: Event) => any> {
+    let listenerTypes = this.listeners.get(selector);
+    if(!listenerTypes) {
+      listenerTypes = new Map();
+      this.listeners.set(selector, listenerTypes);
+    }
+
+    let listenersSet = listenerTypes.get(eventName);
+    if(!listenersSet) {
+      listenersSet = new Set();
+      listenerTypes.set(eventName, listenersSet);
+    }
+
+    return listenersSet;
+  }
+
+  addListener (selector: string, eventName: string, listener: (event: Event) => any): void {
+    const listenersSet = this.getSelectorListeners(selector, eventName);
+    listenersSet.add(listener);
+    const nodes = this.node.querySelectorAll(selector);
+    for(const node of Array.from(nodes)) {
+      node.addEventListener(eventName, listener);
+    }
+  }
+
+  removeListener (selector: string, eventName: string, listener: (event: Event) => any): void {
+    const listenersSet = this.getSelectorListeners(selector, eventName);
+    listenersSet.delete(listener);
+    const nodes = this.node.querySelectorAll(selector);
+    for(const node of Array.from(nodes)) {
+      node.removeEventListener(eventName, listener);
+    }
+  }
+
+  private addListenersToTree (): void {
+    this.listeners.forEach((listenerTypes, selector) => {
+      const nodes = this.node.querySelectorAll(selector);
+      for(let i = 0, len = nodes.length; i < len; i++) {
+        const node = nodes[i];
+        listenerTypes.forEach((listeners, eventName) => {
+          listeners.forEach((listener) => {
+            node.removeEventListener(eventName, listener);
+            node.addEventListener(eventName, listener);
+          });
+        });
+      }
+    });
+  }
+
+  private addListenersToElements (elements: Element[]): void {
+    this.listeners.forEach((listenerTypes, selector) => {
+      elements.forEach((element) => {
+        if(element.matches(selector)) {
+          listenerTypes.forEach((listeners, eventName) => {
+            listeners.forEach((callback) => {
+              element.addEventListener(eventName, callback);
+            });
+          });
+        }
+      });
+    });
   }
 
   private triggerDOMUpdate (): void {
@@ -59,7 +127,8 @@ export class DOMController<T=any> {
     this.rafRequest = window.requestAnimationFrame(() => {
       this.rafRequest = null;
       const newTemplateNode = this.builder.getTemplateNode(this.state).content;
-      updateDOM(this.node, newTemplateNode);
+      const stats = updateDOM(this.node, newTemplateNode);
+      this.addListenersToElements(stats.newElements);
     });
   }
 
